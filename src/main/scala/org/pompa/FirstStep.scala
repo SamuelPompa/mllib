3ea7490a25bd97b55c44b49259a9ec292b98b8f7
Samuel <samuelpompa@gmail.com>
2017-07-04 21:55:48 +0200

    mllib maven dependency

--- src/main/scala/org/pompa/FirstStep.scala
+++ src/main/scala/org/pompa/FirstStep.scala
@@ -4,7 +4,7 @@ import org.apache.spark.sql.SparkSession
 import org.slf4j.LoggerFactory
 
 /**
-  * Created by utad on 4/07/17.
+  * Created by Samuel on 4/07/17.
   */
 object FirstStep {
 
@@ -15,16 +15,27 @@ object FirstStep {
     val ss = SparkSession.builder()
       .master("local[*]")
       .appName("FirstStep").getOrCreate()
-    val rdd = ss.sparkContext.textFile("src/main/resources/foods_prueba.txt")
 
-    rdd.take(4).foreach(x => println(x))
+
+
+    val rdd = ss.sparkContext.textFile("src/main/resources/foods_prueba.txt")
+    LOGGER.debug(s"NumPartitions ${rdd.getNumPartitions}")
+    val hugeRdd2 = ss.sparkContext.textFile("/home/utad/Escritorio/Datos/foods.txt")
+
+    rdd.take(10).foreach(x => println(x))
+
+    LOGGER.debug(s"Numero de filas antes de memoria en fichero: ${hugeRdd2.count}")
+    hugeRdd2.cache
+    LOGGER.debug(s"Numero de filas despues de memoria en fichero: ${hugeRdd2.count}")
+    LOGGER.debug(s"Numero de filas despues de memoria en fichero: ${hugeRdd2.count}")
 
     val count = rdd.count
 
     LOGGER.debug(s"Numero de filas en fichero: $count")
     LOGGER.error(s"Numero de filas en fichero: $count")
 
-
+    LOGGER.debug(s"Numero de filas en fichero: ${hugeRdd2.count}")
+    while(true){}
 
   }
 
